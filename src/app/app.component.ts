b868ea525179b075a9c6c874dccdd3aedccb43b3
jessicagomes-git <jessicagomes802@gmail.com>
2021-01-02 22:58:57 -0300

    Commit inicial do controle de lancamentos estatico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'algamoney-ui';
+
+  lancamentos = [
+    {
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
+    },
+    {
+      tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017',
+      dataPagamento: null, valor: 8000, pessoa: 'Atacado Brasil'
+    },
+    {
+      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '15/09/2017',
+      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda'
+    },
+  ];
+
 }
