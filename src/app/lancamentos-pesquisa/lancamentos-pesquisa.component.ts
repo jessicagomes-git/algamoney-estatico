6fb6ddb30acb17d034d30f8e677ca6bc4a4f6d8e
jessicagomes-git <jessicagomes802@gmail.com>
2021-01-21 21:02:19 -0300

    Criando componentes de cadastro de lancamentos e pessoas

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -14,17 +14,17 @@ export class LancamentosPesquisaComponent implements OnInit {
 
   lancamentos = [
     {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: '28/06/2017', valor: 4.55, pessoa: 'Padaria do José'
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 6 ,30),
+      dataPagamento: new Date(2017, 6 ,28), valor: 4.55, pessoa: 'Padaria do José'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017',
-      dataPagamento: '10/06/2017', valor: 8000, pessoa: 'Atacado Brasil'
+      tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: new Date(2017, 6 ,10),
+      dataPagamento: new Date(2017, 6 ,10), valor: 8000, pessoa: 'Atacado Brasil'
     },
     {
-      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '15/09/2017',
-      dataPagamento: '15/09/2017', valor: 14312, pessoa: 'Ministério da Fazenda'
+      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2017, 9, 15),
+      dataPagamento: new Date(2017, 9, 15), valor: 14312, pessoa: 'Ministério da Fazenda'
     },
   ];
-  
+
 }
